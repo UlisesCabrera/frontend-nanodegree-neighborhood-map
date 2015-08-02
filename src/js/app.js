118f57551cc26fb8650094487c15994c86ff7a40
Ulises Cabrera <ucabrera@outlook.com>
2015-08-01 21:26:28 -0400

    Added Asynchronous Loading on MAP API

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,19 @@
 var model = {
-		loadMap : function (){
-			var mapOptions = {
-      			center: { lat:40.8621822, lng:-73.8935974},
-      			zoom: 18
-   			 };
-    		var map = new google.maps.Map(document.getElementById('mapCanvas') , mapOptions);			
-		}
-	};
+	init: function(){
+		$('body').append('<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-ea-0b-EOXWC6svLpOyxcBKs3ecfe1co&callback=model.loadMap">');	
+	},
+	loadMap : function (){
+		var mapOptions = {
+  			center: { lat:40.8621822, lng:-73.8935974},
+  			zoom: 18
+		 };
+		var map = new google.maps.Map(document.getElementById('mapCanvas') , mapOptions);			
+	}	
+};
 	
-	var ViewModel = function() {
-		var self = this;
-		self.map = ko.observable(model.loadMap());
-	};
+var ViewModel = function() {
+	var self = this;
+	self.map = ko.observable(model.init());
+};
 	
-	ko.applyBindings(new ViewModel());
+ko.applyBindings(new ViewModel());
