e543119384770ad9173a5377b8fabbee2fc6a5f5
Ulises Cabrera <ucabrera@outlook.com>
2015-08-06 13:11:13 -0400

    Synchronize markers with the results from the filter process

--- src/js/app.js
+++ src/js/app.js
@@ -24,7 +24,10 @@ var model = {
 		function resultsHandler(results, status) {
 		  if (status == google.maps.places.PlacesServiceStatus.OK) {
 		    for (var i = 0; i < results.length; i++) {	      
+			  
 			  vm.places.push(results[i]);
+			  vm.createMarker(results[i]);
+
 		    };
 		  };
 		}
@@ -35,6 +38,7 @@ var ViewModel = function() {
 	var self = this;
 	self.lat = ko.observable(40.8621822);
 	self.lng = ko.observable(-73.8935974);
+	
 	self.init = function() {
 		var script = document.createElement('script');
 		$(script).attr("type", "text/javascript");
@@ -44,25 +48,39 @@ var ViewModel = function() {
 		script.src = "https://maps.googleapis.com/maps/api/js?libraries=places&signed_in=true&key=AIzaSyD-ea-0b-EOXWC6svLpOyxcBKs3ecfe1co&callback=model.loadMap";
 		$('body').append(script);
 	};
+
 	// empty array that will hold the results from the nearbySearch
 	self.places = ko.observableArray([]);
+	
+	// empty array that will hold the markers
+	self.markers = ko.observableArray([]);
+
 	self.query = ko.observable('');
+	//filtered arrays 
 	self.search = ko.computed(function(){
-    return ko.utils.arrayFilter(self.places(), function(point){	
-      self.createMarker(point);
-	  return point.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+    return ko.utils.arrayFilter(self.markers(), function(point){	
+	  return point.title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
    		});
   	});
+
+  	// runs the setMarkers function each time the search array changes
+  	self.search.subscribe(function(){
+  		self.setMarkers();
+  	});
+
 	// create markers function from the results of the search
 	self.createMarker = function(place) {
+		  // get location from results
 		  var placeLoc = place.geometry.location;
 		  // get photos if they are avaiable
 		  var photos = (!place.photos) ? 'images/sample.jpg' : place.photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200});
+		  // set initial markers based on the restults
 		  var marker = new google.maps.Marker({
 		    map: map(),
 		    position: placeLoc,
-			title: place.name,
+			title: place.name
 		  });
+		  //create info window
 		  var infowindow = new google.maps.InfoWindow({
 			 content : '<h4 class="text-center">' + place.name + '</h4>' + '<img src="'+ photos +'" class="img-responsive">'
 		  });
@@ -70,7 +88,27 @@ var ViewModel = function() {
 		  google.maps.event.addListener(marker, 'click', function() {
 	   		infowindow.open(map(), this);
 	  		});
-		  }		  		  
+		  //push markers to empty array
+		  self.markers.push(marker);
+		  }
+	//deletes all the markers in the map
+	self.deleteMarkers = function() {
+		for (var i = 0; i < self.markers().length; i++){
+			self.markers()[i].setMap(null);
+		}
+	}
+	
+	self.setMarkers = function() {
+		/* 
+		Deletes all the markers from map first,
+		then check how many markers are in the search array
+		and place them into the map,
+		*/
+		self.deleteMarkers();
+		for (var i = 0; i < self.search().length; i++){
+			self.search()[i].setMap(map());
+		}
+	};  		  		  
 	self.init();
 };
 
